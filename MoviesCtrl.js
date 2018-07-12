23b9a0e8ab92b2b17632ba30abdd3009e16c5762
Igalram <ram8555@gmail.com>
2018-07-12 12:19:30 +0300

    moving controller functions to service

--- MoviesCtrl.js
+++ MoviesCtrl.js
@@ -1,63 +1,26 @@
 app.controller('moviesCtrl', function ($scope, $http, convertService, favMoviesSrv) {
 
-  
-
   $scope.movies = [];
 
 
   //constructor
-    function Movie(name, length, snippet, imdb, poster, id) {
-      this.name = name;
-      this.length = length;
-      this.snippet = snippet;
-      this.imdb = imdb;
-      this.poster = poster;
-      this.id = id;
-  
-    }
-  /*
-  //hardcorded movie
-    var movie = new Movie("The Rocky Horror Picture Show", 100, "chunky productions", "https://www.themoviedb.org/movie/36685-the-rocky-horror-picture-show?language=en-US", "https://image.tmdb.org/t/p/w600_and_h900_bestv2/v2NC7o8f7AZvQbOAwrfRbe5Z106.jpg");
-    $scope.movies.push(movie);
-    var movie = new Movie("The Rocky Horror Picture Show", 100, "chunky productions", "https://www.themoviedb.org/movie/36685-the-rocky-horror-picture-show?language=en-US", "https://image.tmdb.org/t/p/w600_and_h900_bestv2/v2NC7o8f7AZvQbOAwrfRbe5Z106.jpg");
-    $scope.movies.push(movie);
-    var movie = new Movie("The Rocky Horror Picture Show", 100, "chunky productions", "https://www.themoviedb.org/movie/36685-the-rocky-horror-picture-show?language=en-US", "https://image.tmdb.org/t/p/w600_and_h900_bestv2/v2NC7o8f7AZvQbOAwrfRbe5Z106.jpg");
-    $scope.movies.push(movie);
-    console.log($scope.movies);
-*/
-  /*
-    $http.get('actors.JSON').then(function(response) {
-      response.data.forEach(function(plainObj) {
-        var actor = new Actor (plainObj.name, plainObj.age, plainObj.imgURL, plainObj.url);
-        $scope.actors.push(actor);
-      })
-  
-  }, function(error) {
-      console.error(error);
-    });
-  */
+  function Movie(name, length, snippet, imdb, poster, id) {
+    this.name = name;
+    this.length = length;
+    this.snippet = snippet;
+    this.imdb = imdb;
+    this.poster = poster;
+    this.id = id;
 
-  
+  }
+
+
+
   $scope.query = "";
   $tempResults = [];
   $scope.wordRate = convertService.wordRate;
   $scope.API_KEY = favMoviesSrv.API_KEY;
 
-  /*
-      $scope.criteriaMatch = function (movie) {
-  
-          if (!$scope.query || movie.name.toLowerCase().includes($scope.query.toLowerCase())) { return true; }
-          else { return false; }
-  
-      };
-  
-      $scope.sorts = [{label: 'Alphabetically', name: 'name'},{label: 'Birthday', name: 'age'}];
-  
-  
-      $scope.movieSelected = null;
-      $scope.setSelected = function (movieSelected) {
-          $scope.movieSelected = movieSelected;
-       };  */
 
   $scope.getResults = function (query) {
     if ($scope.query) {
@@ -70,42 +33,14 @@ app.controller('moviesCtrl', function ($scope, $http, convertService, favMoviesS
         console.log($scope.tempResults);
         $scope.poster = "https://image.tmdb.org/t/p/w200" + $scope.tempResults.poster_path;
 
-      }, 
-      function (error) {
-      console.error(error);
-      })
+      },
+        function (error) {
+          console.error(error);
+        })
     } else {
       $scope.tempResults = [];
     }
   }
-
-  $scope.getMovie = function (result) {
-    var id="";
-    console.log(result.id);
-    var tempMovieObj;
-    id=result.id;
-    var getMovieUrl = "https://api.themoviedb.org/3/movie/" + id + "?api_key=" +
-    $scope.API_KEY + "&language=en-US";
-
-    $http.get(getMovieUrl).then(function (response) {
-    tempMovieObj = response.data.title;
-    var movie = new Movie(response.data.title, 
-                          response.data.runtime, 
-                          response.data.overview,
-                          response.data.homepage,
-                          "https://image.tmdb.org/t/p/w200"+response.data.poster_path,
-                          response.data.id); 
-                          
-    $scope.movies.push(movie);
-
-  }, 
-  function (error) {
-  console.error(error);
-  })
-  $scope.tempResults = [];
-  $scope.query="";
-  
-}
 
 
 
--- favMoviesSrv.js
+++ favMoviesSrv.js
@@ -1,4 +1,4 @@
-app.factory('favMoviesSrv', function() {
+app.factory('favMoviesSrv', function($http, $log, $q, convertService) {
 
     var test= "do you see favMovieservice service???";
     console.log(test);
@@ -9,7 +9,48 @@ app.factory('favMoviesSrv', function() {
 
 //show current favs in movies arr
 
+    var movies = [];
 
+    //constructor
+  function Movie(name, length, snippet, imdb, poster, id) {
+    this.name = name;
+    this.length = length;
+    this.snippet = snippet;
+    this.imdb = imdb;
+    this.poster = poster;
+    this.id = id;
+
+  }
+
+
+    
+  getMovie = function (result) {
+    var id = "";
+    console.log(result.id);
+    var tempMovieObj;
+    id = result.id;
+    var getMovieUrl = "https://api.themoviedb.org/3/movie/" + id + "?api_key=" +
+      $scope.API_KEY + "&language=en-US";
+
+    $http.get(getMovieUrl).then(function (response) {
+      tempMovieObj = response.data.title;
+      var movie = new Movie(response.data.title,
+        response.data.runtime,
+        response.data.overview,
+        response.data.homepage,
+        "https://image.tmdb.org/t/p/w200" + response.data.poster_path,
+        response.data.id);
+
+      movies.push(movie);
+
+    },
+      function (error) {
+        console.error(error);
+      })
+    $scope.tempResults = [];
+    $scope.query = "";
+
+  }
 
 
 //get movie
@@ -17,6 +58,7 @@ app.factory('favMoviesSrv', function() {
        
     return {
          API_KEY :  API_KEY
+
           }
 
 });
